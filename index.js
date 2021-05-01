96ebd307eea6702727ea9455c5e9894c488193c2
Ashutosh Sharma <46045210+Ashutosharmawesome@users.noreply.github.com>
2021-05-01 15:07:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { User } = require('./models/user');
+
 mongoose.connect('mongodb+srv://LuckySharma:abcd3210@cluster0.iuykj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
     {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('DB Connected'))
                             .catch(err => console.error(err));
 
+// app.get('/', (req,res)=>{res.send('hello world')});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get('/', (req,res)=>{
-    res.send('hello world')
-});
+    res.json({"hello ~" : "Hi ~~"})
+})
+
+app.post('/api/users/register', (req, res) =>{
+    
+    const user = new User(req.body)
+    user.save((err, userData) =>{
+        if(err) return res.json({ success: false, err})
+    })
+    return res.status(200).json({
+        success:true
+    })
+})
 
 app.listen(5000);
 
